02b12c37821517befbb5771bdc26121d0e15f5fd
Bahrican Yesil <bahricanyesil@gmail.com>
2021-03-03 23:50:20 +0300

    userId prevention

--- src/utils/helpers/error-helper.js
+++ src/utils/helpers/error-helper.js
@@ -5,15 +5,18 @@ const tr = require('../lang/tr');
 module.exports = (code, req, errorMessage) => {
     //NOTE: This control routes every server error to the same lang key.
     let key = code;
-    if (!en[code]) {
-        key = '00008';
-    }
+    if (!en[code]) key = '00008';
+
+    let userId = '';
+    if (req.user._id) userId = req.user._id;
+
     const enMessage = en[key];
     const trMessage = tr[key];
+
     if (enMessage.includes('server error')) {
-        logger(code, req?.user?._id ?? '', errorMessage, 'Server Error', req);
+        logger(code, userId, errorMessage, 'Server Error', req);
     } else {
-        logger(code, req?.user?._id ?? '', errorMessage ?? enMessage, 'Client Error', req);
+        logger(code, userId, errorMessage ?? enMessage, 'Client Error', req);
     }
 
     return {
